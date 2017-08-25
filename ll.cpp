0470cbbbf0a55f255cee30b5463156b90bf46dc1
blefev <blefev>
2017-08-24 23:16:41 -0600

    Initial commit

---
+++ ll.cpp
@@ -0,0 +1,42 @@
+#include "ll.h"
+
+LL::LL()
+{
+	head = NULL;
+}
+
+void LL::prepend(int num)
+{
+
+}
+
+void LL::append(int num)
+{
+
+}
+
+void LL::remove(int num)
+{
+
+}
+
+bool LL::removeFront()
+{
+
+}
+
+
+bool LL::removeBack()
+{
+
+}
+
+node* LL::search(int num)
+{
+
+}
+
+void print()
+{
+
+}
