98b3d3ff70a05a7774c560c5028a20c59ca2b763
blefev <blefev>
2017-08-28 22:09:56 -0600

    Append works

--- ll.cpp
+++ ll.cpp
@@ -5,19 +5,45 @@ LL::LL()
 	head = NULL;
 }
 
-void LL::prepend(int num)
+bool LL::prepend(int num)
 {
+	node * newNode = new node;
+	newNode->next = this->head;
 
+	this->head = newNode;
 }
 
-void LL::append(int num)
+bool LL::append(int num)
 {
+	node * newNode = new node;
+	if(newNode == NULL)
+		return false;
 
+	newNode->val = num;
+	newNode->next = NULL;
+
+	if(this->head == NULL){
+		this->head = newNode;
+		return true;
+	}
+
+	node * nodePtr = this->head;
+
+
+	while(nodePtr->next != NULL)
+		nodePtr = nodePtr->next;
+
+	nodePtr->next = newNode;
+
+	return true;
 }
 
-void LL::remove(int num)
+bool LL::remove(int num)
 {
-
+	// List empty?
+	if(!head){
+		
+	}
 }
 
 bool LL::removeFront()
@@ -36,7 +62,22 @@ node* LL::search(int num)
 
 }
 
-void print()
+void LL::print()
 {
+	node * cur = this->head;
+	while(cur){
+		cout << cur->val << endl;
+	}
+}
 
+int LL::getSize()
+{
+	node * nodePtr = this->head;
+
+	int listSize = 0;
+
+	while(nodePtr)
+		listSize++;
+
+	return listSize;
 }
---
+++ main2.cpp
@@ -0,0 +1,19 @@
+
+#include <fstream>          // Include to use ifstream
+#include "ll.h"             // Include so can access our class
+using namespace std;        // Include so we don't need to put std:: infront
+                            // of cout and endl
+
+int main()
+{
+	LL myList;
+	
+	for(int i = 0; i <= 10; i++){
+		cout << "Appending " << i << " to list." << endl;
+		myList.append(i);
+	}
+
+	//cout << "List size is: "<< myList.getSize() << endl;
+	
+//	myList.print();
+}
